857d23baaa0f77b352a0ede4db87ab9653e8e0bb
Monace1 <morganchuma654@gmail.com>
2024-07-23 16:45:23 +0300

    Spring boot backend

---
+++ giftApp/src/main/java/com/gift/giftApp/GiftAppApplication.java
@@ -0,0 +1,13 @@
+package com.gift.giftApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftAppApplication.class, args);
+	}
+
+}
---
+++ giftApp/src/test/java/com/gift/giftApp/GiftAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gift.giftApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
